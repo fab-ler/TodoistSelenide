4cd8d462fe489490038d7dc57f74388318906e38
Юлия <yanju@tut.by>
2020-05-11 00:44:48 +0300

    TestListener.java upd

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,47 +1,63 @@
 package utils;
 
 import io.qameta.allure.Attachment;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
+import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestListener {
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+@Log4j2
+public class TestListener implements ITestListener {
+
     public void onTestStart(ITestResult iTestResult) {
-        System.out.println(String.format("======================================== STARTING TEST %s ========================================", iTestResult.getName()));
+        log.info(String.format("======================================== STARTING TEST %s ========================================", iTestResult.getName()));
     }
 
     public void onTestSuccess(ITestResult iTestResult) {
-        System.out.println(String.format("======================================== FINISHED TEST %s Duration: %ss ========================================", iTestResult.getName(),
+        log.info(String.format("======================================== FINISHED TEST %s Duration: %ss ========================================", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
     }
 
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.println(String.format("======================================== FAILED TEST %s Duration: %ss ========================================", iTestResult.getName(),
+        log.error(String.format("======================================== FAILED TEST %s Duration: %ss ========================================", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
-        takeScreenshot(iTestResult);
     }
 
     public void onTestSkipped(ITestResult iTestResult) {
-        System.out.println(String.format("======================================== SKIPPING TEST %s ========================================", iTestResult.getName()));
-        takeScreenshot(iTestResult);
+        log.warn(String.format("======================================== SKIPPING TEST %s ========================================", iTestResult.getName()));
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+        log.info(String.format("======================================== FAILED TEST %s Duration: %ss ========================================", iTestResult.getName(),
+                getExecutionTime(iTestResult)));
+    }
+
+    public void onStart(ITestContext context) {
+        log.info("======================================== STARTING TESTS ========================================");
+    }
+
+    public void onFinish(ITestContext context) {
+        log.info("======================================== TESTS FINISHED ========================================");
+
     }
 
     @Attachment(value = "Last screen state", type = "image/png")
     private byte[] takeScreenshot(ITestResult iTestResult) {
-        ITestContext context = iTestResult.getTestContext();
         try {
-            return ((TakesScreenshot) context.getAttribute("driver")).getScreenshotAs(OutputType.BYTES);
+            return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
         } catch (NoSuchSessionException ex) {
             return null;
         } catch (IllegalStateException ex) {
             return null;
         }
     }
-
 
     private long getExecutionTime(ITestResult iTestResult) {
         return TimeUnit.MILLISECONDS.toSeconds(iTestResult.getEndMillis() - iTestResult.getStartMillis());
