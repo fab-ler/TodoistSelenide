66b9b13815e37be31e95157d04c926ad585b536f
Юлия <yanju@tut.by>
2020-05-10 16:32:18 +0300

    TodoistTest.java broken into multiple steps commented Aftermethon and added configuration holdOpenBrowser for test purposes

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import steps.TodoistSteps;
-
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class BaseTest {
 
@@ -18,6 +15,8 @@ public class BaseTest {
         Configuration.timeout = 10000;
         Configuration.startMaximized = true;
         Configuration.headless = false;
+        //TODO remove later
+        Configuration.holdBrowserOpen = true;
     }
 
     @BeforeMethod
@@ -25,9 +24,9 @@ public class BaseTest {
         steps = new TodoistSteps();
     }
 
-    @AfterMethod
-    public void closeBrowser() {
-        getWebDriver().close();
-    }
+//    @AfterMethod
+//    public void closeBrowser() {
+//        getWebDriver().close();
+//    }
 
 }
--- src/test/java/tests/TodoistTest.java
+++ src/test/java/tests/TodoistTest.java
@@ -1,21 +1,38 @@
 package tests;
 
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 
 public class TodoistTest extends BaseTest {
 
-    @Test
-    public void createNewTask() {
+
+    @Step("check for signin error messages, when using wrong credentials")
+    @Test(priority = 0)
+    // open login page
+    // try to sign in with invalid credentials
+    public void wrongCredsErrorCheck() {
+        steps
+                .wrongCredsErrorCheck();
+    }
+
+    @Step("successful sign in")
+    @Test(priority = 1)
+    public void signIn() {
         // open login page
-        // login
+        // sign in with correct credentials
+        steps
+                .signIn("tmsaqa3@gmail.com", "pswrdAQA3");
+    }
+
+    @Step("add several new tasks and check incoming tasks")
+    @Test(priority = 2)
+    public void createNewTask() {
         // click add task
         // add three tasks with three different terms
         // edit an existing task (priority, project)
         // fulfill task
         // delete task
         steps
-                .wrongCredsErrorCheck()
-                .signIn("tmsaqa3@gmail.com", "pswrdAQA3")
                 .newTaskAdd("A Task for today", 0)
                 .newTaskAdd("A task for tomorrow", 1)
                 .newTaskAdd("A Task for a next week", 2)
