29c397d29c795bbf3b6e6c260a000fd86a454649
Юлия <yanju@tut.by>
2020-05-12 22:05:09 +0300

    added  $(LOADER_ICON).should(Condition.disappear); condition do signIn method in LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -5,7 +5,6 @@ import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import tests.utils.CustomDataProvider;
 
 import static com.codeborne.selenide.Selenide.*;
 
